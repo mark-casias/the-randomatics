e5afd2703858ee58ac1408cbb61bc54cfe1b4484
Mark Casias <mark.casias@lullabot.com>
2024-12-26 11:21:46 -0700

    fuck the base theme js

--- web/themes/the_randotheme/js/the-randotheme.js
+++ web/themes/the_randotheme/js/the-randotheme.js
@@ -2,16 +2,3 @@
  * @file
  * the_randotheme behaviors.
  */
-(function (Drupal) {
-
-  'use strict';
-
-  Drupal.behaviors.theRandotheme = {
-    attach (context, settings) {
-
-      console.log('It works!');
-
-    }
-  };
-
-}) (Drupal);
